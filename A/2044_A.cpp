0ce28e7034b6ec1d78998bf9f472c3a4ba046fe5
Saurabh Tribhuvan <satdocs28@gmail.com>
2026-08-13 19:42:04 +0530

    .

---
+++ A/2044_A.cpp
@@ -0,0 +1,33 @@
+/*
+A. Easy Problem
+https://codeforces.com/problemset/problem/2044/A
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+void solve()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        cout << n - 1 << '\n';
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
