a0dc1911e3221aef26521e5bbd917c7fd58422d5
Saurabh Tribhuvan <satdocs28@gmail.com>
2026-08-19 12:19:13 +0530

    .

---
+++ A/2009_A.cpp
@@ -0,0 +1,33 @@
+/*
+A. Minimize!
+https://codeforces.com/problemset/problem/2009/A
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+void solve()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        cout << b - a << '\n';
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
