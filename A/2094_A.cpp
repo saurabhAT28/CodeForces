441f0fa51c9fe842404bf59737a8dae62529700d
Saurabh Tribhuvan <satdocs28@gmail.com>
2026-09-03 14:10:52 +0530

    .

---
+++ A/2094_A.cpp
@@ -0,0 +1,33 @@
+/*
+A. Trippi Troppi
+https://codeforces.com/problemset/problem/2094/A
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+void solve()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        string a, b, c;
+        cin >> a >> b >> c;
+
+        cout << a[0] << b[0] << c[0] << '\n';
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
