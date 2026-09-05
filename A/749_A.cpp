e15a63fa9d9118518b1ca48d7a0bfb86674dd086
Saurabh Tribhuvan <satdocs28@gmail.com>
2026-09-05 10:06:43 +0530

    .

---
+++ A/749_A.cpp
@@ -0,0 +1,24 @@
+/*
+A. Bachgold Problem
+https://codeforces.com/problemset/problem/749/A
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
